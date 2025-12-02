db871148fedd1c95bb242ee60ab577e0ce5b315a
Cavani-Padron <luisgabrielpadron@gmail.com>
2025-12-01 20:54:18 -0600

    Creacion de las pantallas; Nueva contraseña, perfil_proveedor_edit, contraseña_seguridad_cambio

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,15 @@
-// import { useState } from 'react';
-// import Contraseña_de_seguridad_del_cambio from '../../screens/Contrasena_de_seguridad_del_cambio';
-// import NuevaContraseña from '../../screens/NuevaContraseña';
-
-// export default function Index() {
-//   const [step, setStep] = useState(1);
-
-//   if (step === 1) {
-//     return (
-//       <Contraseña_de_seguridad_del_cambio onContinue={() => setStep(2)} />
-//     );
-//   }
-
-//   return <NuevaContraseña />;
-// }
-// }
-// import Perfil_Proveedor_Edit from '../../screens/Perfil_Proveedor_Edit';
-
-// export default function Index() {
-//   return <Perfil_Proveedor_Edit />;
-// }
-
-import Lupa from '../../screens/Lupa';
+import { useState } from 'react';
+import Contraseña_de_seguridad_del_cambio from '../../screens/Contrasena_de_seguridad_del_cambio';
+import NuevaContraseña from '../../screens/NuevaContraseña';
 
 export default function Index() {
-  return <Lupa/>;
+const [step, setStep] = useState(1);
+
+if (step === 1) {
+     return (
+       <Contraseña_de_seguridad_del_cambio onContinue={() => setStep(2)} />
+     );
+   }
+
+   return <NuevaContraseña />;
 }
